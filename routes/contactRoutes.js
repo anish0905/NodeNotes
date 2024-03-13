3b1b300931e7b2372788058a0d5665a43ebd4cb1
anish0905 <anishanish0102@gmail.com>
2024-03-12 22:21:30 -0900

    Create a multiple router

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -5,4 +5,32 @@ router.route('/').get((req,res)=>{
     res.status(200).json({message:"Get all contacts"});
 
 })
+
+
+// post
+
+router.route('/').post((req,resp)=>{
+resp.status(200).json({message:"Create contact"})
+});
+
+
+// Get?
+
+router.route('/:id').get((req,resp)=>{
+    resp.status(200).json({message:`Get contact for ${req.params.id}`});
+})
+
+// put@\
+
+router.route('/:id').put((req,resp)=>{
+    resp.status(200).json({message:`update the contact for ${req.params.id}`});
+})
+
+
+// delete
+
+router.route('/:id').delete((req,resp)=>{
+    resp.status(200).json({message:`Delete contact for ${req.params.id}`})
+});
+
 module.exports=router
